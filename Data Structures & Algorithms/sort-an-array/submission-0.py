aba5d9466543cd2cbbecef15b87fafa5c8ed9317
Arka Garai <arkagarai292@gmail.com>
2026-08-27 12:22:42 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,49 @@
+class Solution:
+
+    def merge(self, arr, l, mid, r):
+        i, j = l, mid + 1
+        help = []
+
+        while i <= mid and j <= r:
+            if arr[i] <= arr[j]:
+                help.append(arr[i])
+                i += 1
+            else:
+                help.append(arr[j])
+                j += 1
+
+        while i <= mid:
+            help.append(arr[i])
+            i += 1
+
+        while j <= r:
+            help.append(arr[j])
+            j += 1
+
+        for num in help:
+            arr[l] = num
+            l += 1
+
+
+    def mergeSort(self,arr,l,r):
+        if l >= r:
+            return
+
+        #divide and conquer
+        mid = (l + r) // 2
+        #recursively sort left half
+        self.mergeSort(arr,l,mid)    
+        #recursively sort right half
+        self.mergeSort(arr,mid+1,r)    
+
+        #merge
+        self.merge(arr,l,mid,r)
+
+
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.mergeSort(nums,0,len(nums)-1)
+        return nums
+
+
+        
+        
